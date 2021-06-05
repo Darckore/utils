f49fded36cff035e9130881d87f2bd903d70b86f
darckore <darckore@gmail.com>
2021-06-06 00:36:00 +0300

    added class macros

--- utils.hpp
+++ utils.hpp
@@ -1,1 +1,53 @@
 #pragma once
+/* This thing is to be included somewhere.
+ * Probably, the "somewhere" in question has precompiled headers and stuff.
+ * std includes are intentionally absent here, I assume they come from someplace
+ * and are perfectly accessible on compilation.
+*/
+
+#pragma region Macros for special class members
+#define CLASS_SPECIALS_ALL(clName)\
+  clName() = default;\
+  clName(const clName&) = default;\
+  clName& operator=(const clName&) = default;\
+  clName(clName&&) = default;\
+  clName& operator=(clName&&) = default;\
+  ~clName() = default
+
+#define CLASS_SPECIALS_NOCOPY(clName)\
+  clName() = default;\
+  clName(const clName&) = delete;\
+  clName& operator=(const clName&) = delete;\
+  clName(clName&&) = default;\
+  clName& operator=(clName&&) = default;\
+  ~clName() = default
+
+#define CLASS_SPECIALS_NODEFAULT(clName)\
+  clName() = delete;\
+  clName(const clName&) = default;\
+  clName& operator=(const clName&) = default;\
+  clName(clName&&) = default;\
+  clName& operator=(clName&&) = default;\
+  ~clName() = default
+
+#define CLASS_SPECIALS_NODEFAULT_NOCOPY(clName)\
+  clName() = delete;\
+  clName(const clName&) = delete;\
+  clName& operator=(const clName&) = delete;\
+  clName(clName&&) = default;\
+  clName& operator=(clName&&) = default;\
+  ~clName() = default
+
+#define CLASS_SPECIALS_NONE(clName)\
+  clName() = delete;\
+  clName(const clName&) = delete;\
+  clName& operator=(const clName&) = delete;\
+  clName(clName&&) = delete;\
+  clName& operator=(clName&&) = delete;\
+  ~clName() = default
+#pragma endregion
+
+namespace utils
+{
+  
+}

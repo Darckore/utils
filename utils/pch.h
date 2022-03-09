4a939ab7303bf24a2ae879105c0325d857481474
darckore <darckore@gmail.com>
2022-03-09 14:15:51 +0300

    ++cmake

---
+++ utils/pch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// std headers we'll most likely be using
+#include <type_traits>
+#include <utility>
+#include <source_location>
+
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <string_view>
+
+#include <array>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+#include <filesystem>
+#include <memory>
+#include <stdexcept>
+
+// just because it is often needed everywhere
+using namespace std::literals;

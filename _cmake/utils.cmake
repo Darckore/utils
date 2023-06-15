5e617d0db9ca378d75bf76a992a9842373f9f15d
darckore <darckore@gmail.com>
2023-06-15 19:49:47 +0300

    VS modules workaround

--- _cmake/utils.cmake
+++ _cmake/utils.cmake
@@ -11,7 +11,9 @@ function(collect_sources SOURCE_FILES HEADERS ADDITIONAL_FILES)
   file( GLOB_RECURSE SRC
         LIST_DIRECTORIES false      
         "${CMAKE_CURRENT_SOURCE_DIR}/include/*.h*"
-        "${CMAKE_CURRENT_SOURCE_DIR}/include/*/*.h*" )
+        "${CMAKE_CURRENT_SOURCE_DIR}/include/*.inl"
+        "${CMAKE_CURRENT_SOURCE_DIR}/include/*/*.h*"
+        "${CMAKE_CURRENT_SOURCE_DIR}/include/*/*.inl")
   
   set(HEADERS "${SRC}" PARENT_SCOPE)
   set(SRC)
@@ -51,6 +53,18 @@ function(set_build_opts TARGET_NAME EXCLUDED_FILES)
     set_target_properties( ${TARGET_NAME} PROPERTIES
                            VS_DEBUGGER_WORKING_DIRECTORY "$<TARGET_FILE_DIR:${TARGET_NAME}>")
     set_property(SOURCE ${EXCLUDED_FILES} PROPERTY VS_SETTINGS "ExcludedFromBuild=true")
+
+    #vs modules workaround
+    file( CONFIGURE OUTPUT "${CMAKE_BINARY_DIR}/Directory.Build.props" CONTENT [==[
+          <Project>
+            <ItemDefinitionGroup>
+              <ClCompile>
+                <BuildStlModules>false</BuildStlModules>
+              </ClCompile>
+            </ItemDefinitionGroup>
+          </Project>
+          ]==] @ONLY )
+
   else()
     target_compile_options(${TARGET_NAME} PRIVATE -Wall -Wextra -pedantic -Werror)
   endif()

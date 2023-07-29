e9ac9e111b916bdaf375d352dff0d1c963b9fe59
darckore <darckore@gmail.com>
2023-07-29 17:19:45 +0300

    removed the timer, it was garbage

--- utils/include/utils/detail/clock.hpp
+++ utils/include/utils/detail/clock.hpp
@@ -9,14 +9,14 @@ namespace utils
   class clock
   {
   public:
-    using value_type = T;
-    using clock_t = Clock;
+    using value_type   = T;
+    using clock_t      = Clock;
     using time_point_t = typename clock_t::time_point;
-    using duration_t = chrono::duration<value_type>;
+    using duration_t   = chrono::duration<value_type>;
 
+    CLASS_SPECIALS_NONE_CUSTOM(clock);
     clock() = default;
     ~clock() = default;
-    CLASS_SPECIALS_NONE_CUSTOM(clock);
 
     auto operator()() noexcept
     {
@@ -66,140 +66,4 @@ namespace utils
     time_point_t m_mark = clock_t::now();
   };
 
-  //
-  // Determines how the timer should be executed:
-  // - automatic: starts immediately, restart is automatic
-  //              until stop is called or the object is destroyed
-  // - deferred:  need to call start to run, restart is automatic
-  //              until stop is called or the object is destroyed
-  // - manual:    need to call start to run and restart
-  //
-  enum class timer_policy : uint8_t
-  {
-    automatic,
-    deferred,
-    manual
-  };
-
-  //
-  // A timer. Calls a function ashynchronously at the given interval
-  //
-  template <typename F>
-  class timer
-  {
-  public:
-    using interval_unit_t = chrono::milliseconds;
-    using clock_t = clock<double>;
-    using underlying_type = clock_t::value_type;
-    using callback_t = F;
-    using exec_t = std::future<void>;
-
-    using enum timer_policy;
-
-  public:
-    CLASS_SPECIALS_NONE(timer);
-    ~timer() noexcept
-    {
-      stop();
-    }
-
-    timer(timer_policy policy, interval_unit_t interval, callback_t callback) :
-      m_interval{ interval },
-      m_callback{ std::move(callback) },
-      m_policy{ policy }
-    {
-      if (is_autostart())
-        start();
-    }
-
-    timer(interval_unit_t interval, callback_t callback) :
-      timer{ deferred, interval, std::move(callback) }
-    {}
-
-    void set_interval(interval_unit_t interval) noexcept
-    {
-      m_interval = interval;
-    }
-    void start()
-    {
-      stop();
-      reset_clock();
-      m_active = true;
-      auto new_exec = std::async(std::launch::async,
-        [this]
-        {
-          tick();
-        });
-      
-      m_execution = std::move(new_exec);
-    }
-    void stop() 
-    {
-      m_active = false;
-    }
-
-    bool is_autostart() const noexcept
-    {
-      return m_policy == automatic;
-    }
-    bool is_deferred() const noexcept
-    {
-      return m_policy == deferred;
-    }
-    bool is_manual() const noexcept
-    {
-      return m_policy == manual;
-    }
-
-  private:
-    void callback() noexcept(std::is_nothrow_invocable_v<callback_t>)
-    {
-      constexpr auto noparInv = std::is_invocable_r_v<void, callback_t>;
-      constexpr auto paramInv = std::is_invocable_r_v<void, callback_t, decltype(*this)&>;
-      static_assert(noparInv || paramInv, "Invalid callback signature");
-
-      if constexpr (paramInv)
-      {
-        m_callback(*this);
-      }
-      else
-      {
-        m_callback();
-      }
-    }
-    void mark()
-    {
-      callback();
-      if (is_manual())
-      {
-        stop();
-      }
-      reset_clock();
-    }
-    void reset_clock()
-    {
-      m_clock();
-    }
-    void tick()
-    {
-      for (;;)
-      {
-        if (!m_active)
-          return;
-
-        if (m_clock.peek<interval_unit_t>() >= m_interval)
-        {
-          mark();
-        }
-      }
-    }
-
-  private:
-    callback_t m_callback;
-    interval_unit_t m_interval;
-    exec_t m_execution;
-    clock_t m_clock;
-    std::atomic_bool m_active = false;
-    timer_policy m_policy;
-  };
 }
